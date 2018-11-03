d2c0fda0889b11464ead00283dd1a94de7083ae6
Jason Lin <jasonlin43212@gmail.com>
2018-11-03 13:30:36 -0400

    did most of the ls -l structure

--- dirread.c
+++ dirread.c
@@ -6,20 +6,64 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <grp.h>
+#include <pwd.h>
 
 //check error no
-void print_sizebytes(long bytes){
+void print_sizebytes(long long bytes){
   char * names[9] = {"B","KB","MB","GB","TB","PB","EB","ZB","YB"};
   int sizeofboi = 0;
   while(bytes > 1000){
     bytes /= 1000;
     sizeofboi++;
   }
-  printf("%ld %s\n",bytes,names[sizeofboi]);
+  printf("%lld %s\n",bytes,names[sizeofboi]);
+}
+
+void tree_branch(int i) {
+  int j;
+  for (j=0; j<i; j++){
+    printf("| ");
+  }
+}
+
+void to_rwx(int permissions, char * output) {
+  int i;
+  char temp[10];
+  char * possible_rwx [8] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
+  strcat(output,"-");
+  for (i=0; i<3; i++){
+    strcat(output,possible_rwx[permissions%8]);
+    permissions /= 8;
+  }
+  strcpy(temp, output);
+}
+
+void timify(char * old_time, char * output){
+  old_time += 3;
+  strncpy(output,old_time,13);
+}
+
+long long print_file_details(char * filename){
+  struct stat file_stat;
+  char file_name[strlen(filename)];
+  strcpy(file_name,filename);
+
+  stat(filename,&file_stat);
+  struct passwd * pw = getpwuid(file_stat.st_uid);
+  struct group * gr = getgrgid(file_stat.st_gid);
+
+  char rwx_output[12] = "";
+  to_rwx(file_stat.st_mode, rwx_output);
+
+  char time[16] = "";
+  timify(ctime(&file_stat.st_atime),time);
+  printf("%s %s %s %ld %s %s",rwx_output,pw->pw_name,gr->gr_name,file_stat.st_size,time,file_name);
+  return file_stat.st_size;
 }
 
 //two passes
-void print_dir(char * filename){
+long long print_dir(char * filename, int depth){
   // First Pass
   DIR * stream = opendir(filename);
   struct dirent * entry = readdir(stream);
@@ -33,14 +77,14 @@ void print_dir(char * filename){
   stream = opendir(filename);
   entry = readdir(stream);
 
-  printf("Statistics for directory: %s\n\n",filename);
+  printf("\nStatistics for directory: %s\n\n",filename);
   int num_directory = 0;
   int num_file = 0;
 
   char * directory_names[total_things];
   char * file_names[total_things];
 
-  int directory_size = 0;
+  long long directory_size = 0;
 
   while (entry){
     //It is a directory
@@ -52,30 +96,32 @@ void print_dir(char * filename){
     else {
       char * current_file_name = entry->d_name;
       file_names[num_file] = current_file_name;
-
-      struct stat file_stat;
-      stat(current_file_name,&file_stat);
-      directory_size += file_stat.st_size;
       num_file++;
     }
     entry = readdir(stream);
   }
   closedir(stream);
 
-  printf("Directories:\nTotal Direcotry Size: ");
-  print_sizebytes(directory_size);
+  printf("\nDirectories:\n ");
   int i;
   for (i=0;i<num_directory;i++){
     printf("%s\n",directory_names[i]);
   }
-  printf("\nRegular Files:\n");
   for (i=0;i<num_file;i++){
     char * current_file_name = file_names[i];
-    printf("%s\n",current_file_name);
+    tree_branch(depth-1);
+    printf("|____");
+    print_file_details(current_file_name);
+    printf("\n");
   }
+
+  printf("Total Direcotry Size: ");
+  print_sizebytes(directory_size);
+
+  return directory_size;
 }
 
 int main() {
-  print_dir("../MKS65C-stat");
+  print_dir(".",1);
   return 0;
 }
